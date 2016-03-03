c940836ac99db19082deff4107602aa4d9c6d517
Ruben Rajkowski <rajkowskiruben@NUBBURIT569AEML.local>
2016-03-02 17:42:48 -0800

    update for video 5 [routes and promops]

--- app/components/Main.js
+++ app/components/Main.js
@@ -4,7 +4,6 @@ var Main = React.createClass({
   render: function () {
     return (
         <div className='main-container'>
-          <div> Hello from Main! </div>
           {this.props.children}
         </div>
     )
