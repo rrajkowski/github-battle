3b5582f8e69d5dea4aa0ba0afe62c075531419d0
Ruben Rajkowski <rajkowskiruben@NUBBURIT569AEML.local>
2016-03-03 18:18:53 -0800

    working github battle app [v10]

--- app/containers/ConfirmBattleContainer.js
+++ app/containers/ConfirmBattleContainer.js
@@ -1,37 +1,40 @@
 var React = require('react');
 var ConfirmBattle = require('../components/ConfirmBattle');
+var githubHelpers = require('../utils/githubHelpers');
 
 var ConfirmBattleContainer = React.createClass({
-  contextTypes:{
+  contextTypes: {
     router: React.PropTypes.object.isRequired
   },
   getInitialState: function () {
-    console.log("getInitialState");
     return {
-        isLoading: true,
-        playerInfo: []
+      isLoading: true,
+      playersInfo: [],
     }
   },
-  componentWillMount: function(){
-    console.log("componentWillMount");
-
-  },
   componentDidMount: function () {
-    console.log("componentDidMount");
     var query = this.props.location.query;
-    // Fetch info from github then update state
+    githubHelpers.getPlayersInfo([query.playerOne, query.playerTwo])
+        .then(function (players) {
+          this.setState({
+            isLoading: false,
+            playersInfo: [players[0], players[1]]
+          })
+        }.bind(this))
   },
-  componentWillReceiveProps: function(){
-    console.log("componentWillReceiveProps");
-  },
-  componentWillUnMount: function(){
-    console.log("componentWillUnMount");
-
+  handleInitiateBattle: function () {
+    this.context.router.push({
+      pathname: '/results',
+      state: {
+        playersInfo: this.state.playersInfo
+      }
+    })
   },
   render: function () {
     return (
         <ConfirmBattle
             isLoading={this.state.isLoading}
+            onInitiateBattle={this.handleInitiateBattle}
             playersInfo={this.state.playersInfo} />
     )
   }
--- app/styles/index.js
+++ app/styles/index.js
@@ -1,6 +1,14 @@
 var styles = {
   transparentBg: {
     background: 'transparent'
+  },
+  space: {
+    marginTop: '25px'
+  },
+  avitar:{
+    maxWidth: '300px',
+    maxHeight: '300px',
+    margin: '0 auto'
   }
 };
 
