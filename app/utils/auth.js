f4bd85cc76ca336943f7ca8a757e0b957198f571
Ruben Rajkowski <rajkowskiruben@NUBBURIT569AEML.local>
2016-03-04 16:01:55 -0800

    add auth file for heroku

---
+++ app/utils/auth.js
@@ -0,0 +1,10 @@
+var React = require('react');
+
+function auth(){
+  return {
+    id: "3d4f26086659fb9ca705",
+    sec: "8200977642d765d904e44e06657d143dd9cd81dc"
+  }
+}
+
+module.exports = auth;
