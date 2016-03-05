7ad183df9e969346a6d4ba680d4a40b8b475f572
Ruben Rajkowski <rajkowskiruben@NUBBURIT569AEML.local>
2016-03-04 17:09:26 -0800

    config webpack for heroku

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,25 @@
 var HtmlWebpackPlugin = require('html-webpack-plugin')
 var HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
+  //__webpack_public_path__ : 'http://' + process.env.IP || '0.0.0.0:' + process.env.PORT || '8080',
   template: __dirname + '/app/index.html',
   filename: 'index.html',
-  inject: 'body'
+  inject: 'body',
+
 });
 
 module.exports = {
+  devServer:{
+    contentBase: PATHS.build,
+    historyApiFallback: true,
+    hot: true,
+    inline: true,
+    progress: true,
+    stats: 'errors-only',
+    host: process.env.HOST || '0.0.0.0',
+    port: process.env.PORT || '8080'
+  },
   entry: [
+    //'webpack-dev-server/client?http://0.0.0.0:80',
     './app/index.js'
   ],
   output: {
